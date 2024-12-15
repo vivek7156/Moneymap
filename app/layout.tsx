676805bef0bf87260fd32bb0b46b296c650097ea
“Vivek <viveksingh7156@gmail.com>
2024-12-15 13:42:27 +0530

    prisma migration

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           <RootProviders>
             <main>
               {children}
-              </main>
+            </main>
           </RootProviders>
         </body>
       </html>
